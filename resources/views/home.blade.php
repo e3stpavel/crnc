7385b93962c8ada0b8e0475d5f58e2fa9dc386d8
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2022-03-23 18:34:42 +0200

    Wireframe UI is ready

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,33 +109,53 @@
     </div>
   </div>
 
-  <div class="w-full h-screen py-5.75em flex flex-col items-start">
+  <div class="w-full py-5.75em flex flex-col items-start">
     <h1>
       Exchange rates
     </h1>
 
-    <table class="pt-4em">
-      <thead>
+    <!--<table class="mt-4em text-left w-full">
+      <thead class="border border-transparent border-t-gray border-b-gray border-opacity-60">
         <tr>
-          <th>Currency code</th>
-          <th>Currency name</th>
-          <th>Currency rate</th>
+          <th><h6>Currency code</h6></th>
+          <th><h6>Currency name</h6></th>
+          <th><h6>Currency rate</h6></th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <th class="uppercase">
-            Aud
-          </th>
-          <th>
-            Australian dollar
-          </th>
-          <th>
-            1.45
-          </th>
+          <td class="uppercase flex flex-row gap-x-1em">
+            <div class="w-1.5em h-1.5em rounded-full bg-gray">
+
+            </div>
+            <h3>Aud</h3>
+          </td>
+          <td>
+            <h3>Australian dollar</h3>
+          </td>
+          <td>
+            <h3>1.45</h3>
+          </td>
         </tr>
       </tbody>
-    </table>
+    </table>-->
+
+    <div class="mt-4em text-left w-full flex flex-col border-t border-t-gray border-opacity-60">
+      <div class="row">
+        <div class="td"><h6>Currency code</h6></div>
+        <div class="td"><h6>Currency name</h6></div>
+        <div class="td"><h6>Currency rate</h6></div>
+      </div>      
+      <div class="row">
+        <div class="td flex flex-row gap-x-1em">
+          <div class="w-1.5em h-1.5em rounded-full mt-0.09375em bg-gray"></div>
+          <h3>Aud</h3>
+        </div>
+        <div class="td"><h3>Australian dollar</h3></div>
+        <div class="td"><h3>1.45</h3></div>
+      </div>
+      
+    </div>
   </div>
 </main>
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,31 @@
-<footer>footer</footer>
+<footer>
+  <div class="flex flex-col p-5.75em w-full bg-black text-white border-b border-b-white border-opacity-60">
+    <div class="grid grid-cols-4 gap-x-4em items-baseline border-b border-b-white border-opacity-60">
+      <h1 class="footer-headline">That's all</h1>
+      <h6 class="footer-headline">Sections</h6>
+      <h6 class="footer-headline">Pages</h6>
+      <h6 class="footer-headline">Inquires</h6>
+    </div>
+    <div class="grid grid-cols-4 gap-x-4em pb-2.625em items-baseline border-b border-b-white border-opacity-60">
+      <p class="invisible">Helper</p>
+      <div class="flex flex-col pt-1em gap-y-0.5em">
+        <p class="capitalize font-600">Introduction</p>
+        <p class="capitalize font-600">Currency converter</p>
+        <p class="capitalize font-600">Exchange rates</p>
+      </div>
+      <div class="flex flex-col pt-1em gap-y-0.5em">
+        <p class="capitalize font-600">Estonian Bank</p>
+        <p class="capitalize font-600">Lithuanian Bank</p>
+        <p class="capitalize font-600">Privacy & Policy</p>
+        <p class="capitalize font-600">Terms & Conditions</p>
+      </div>      
+      <div class="pt-1em">
+        <p class="font-600">Don't want to provide an email here :)</p>
+      </div>
+    </div>
+    <div class="flex flex-row w-full justify-between opacity-60 pt-0.5em">
+      <p>e3stpavel (Pavel Mayorov)</p>
+      <p>&copy; 2022 Bruxemburg</p>
+    </div>
+  </div>
+</footer>
